1ef72e140283cc5d34a05138b6509086150d5532
JJ Allaire <jj@rstudio.com>
2021-03-17 13:19:29 -0400

    regenerate docs

--- docs/libs/bootstrap-v5.0.0-beta2/bootstrap.quarto.js
+++ docs/libs/bootstrap-v5.0.0-beta2/bootstrap.quarto.js
@@ -1,5 +1,64 @@
 window.document.addEventListener("DOMContentLoaded", function() {
   
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+  function headerOffset() {
+    // Set an offset if there is are fixed top navbar
+    const headerEl = window.document.querySelector('header.fixed-top');
+    return headerEl.clientHeight;
+  }
+
+  function updateDocumentOffset() {
+    // set body offset
+    const offset = headerOffset()
+    const bodyEl = window.document.body;
+    bodyEl.setAttribute("data-bs-offset", offset);
+    bodyEl.style.paddingTop = offset + "px";  
+
+    // deal with sidebar offsets
+    const sidebars = window.document.querySelectorAll(".sidebar");
+    sidebars.forEach(sidebar => { 
+      if (!window.Headroom || sidebar.classList.contains("sidebar-pinned")) {
+        sidebar.style.top = offset + "px";
+        sidebar.style.maxHeight = 'calc(100vh - ' + offset + 'px)';   
+      } else {
+        sidebar.style.top = "0";
+        sidebar.style.maxHeight = '100vh';   
+      }
+    });
+
+    // link offset
+    let linkStyle = window.document.querySelector("#quarto-target-style");
+    if (!linkStyle) {
+      linkStyle = window.document.createElement('style');
+      window.document.head.appendChild(linkStyle);
+    }
+    while (linkStyle.firstChild) {
+      linkStyle.removeChild(linkStyle.firstChild);
+    }
+    linkStyle.appendChild(window.document.createTextNode(`
+      :target::before {
+        content: "";
+        display: block;
+        height: ${offset}px;
+        margin: -${offset}px 0 0;
+      }`)
+    );
+  }
+
   // move the toc if there is a sidebar
   var toc = window.document.querySelectorAll('nav[role="doc-toc"]');
   var tocSidebar = window.document.getElementById("quarto-toc-sidebar")
@@ -12,16 +71,32 @@ window.document.addEventListener("DOMContentLoaded", function() {
   body.setAttribute("data-bs-spy", "scroll");
   body.setAttribute("data-bs-target", "#quarto-toc-sidebar");
 
+  // initialize headroom
+  var header = window.document.querySelector("#quarto-header");
+  if (header && window.Headroom) {
+    const headroom  = new window.Headroom(header, 
+      { tolerance: 5,
+        onPin: function() {
+          const sidebars = window.document.querySelectorAll(".sidebar");
+          sidebars.forEach(sidebar => {
+            sidebar.classList.add("sidebar-pinned");
+          });
+          updateDocumentOffset();
+        }, 
+        onUnpin: function() {
+          const sidebars = window.document.querySelectorAll(".sidebar");
+          sidebars.forEach(sidebar => {
+            sidebar.classList.remove("sidebar-pinned");
+          });
+          updateDocumentOffset();
+        }});
+    headroom.init();
+  }
+
   // Set an offset if there is are fixed top navbar
-  const navBar = window.document.querySelectorAll('.navbar.fixed-top');
-  let offset = 0;
-  for (let i=0; i< navBar.length; i++) {
-    offset += navBar[i].clientHeight;
-  }
-  if (offset) {
-    body.setAttribute("data-bs-offset", offset * 2);
-  }
-  
+  updateDocumentOffset();
+  window.addEventListener('resize', debounce(updateDocumentOffset, 50));  
+    
   // add nav-link class to the TOC links
   var tocLinks = window.document.querySelectorAll('nav[role="doc-toc"] a');
   for (let i=0; i<tocLinks.length; i++) {
