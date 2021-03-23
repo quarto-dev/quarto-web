ae73ededb3af55f6291ed245a844bb1a039ab284
JJ Allaire <jj@rstudio.com>
2021-03-23 18:15:34 -0400

    regen docs

---
+++ docs/libs/quarto-html/quarto-html.js
@@ -0,0 +1,9 @@
+window.document.addEventListener("DOMContentLoaded", function (event) {
+  const icon = "";
+  const anchorJS = new window.AnchorJS();
+  anchorJS.options = {
+    placement: 'right',
+    icon: icon
+  };
+  anchorJS.add();
+});
