69473854413874f315ceeb7313c459a001fae3a0
JJ Allaire <jj@rstudio.com>
2021-08-17 10:19:08 -0400

    old faithful example tweaks

--- docs/interactive/shiny/_examples/old-faithful/server.R
+++
@@ -1,8 +0,0 @@
-
-function(input, output, session) {
-  output$distPlot <- renderPlot({
-    x <- faithful[, 2]  # Old Faithful Geyser data
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-  })
-}
