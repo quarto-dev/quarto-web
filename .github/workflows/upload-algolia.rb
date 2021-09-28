24eeb7fe519c9208fb14a888e7836a0907dcdf52
Charles Teague <cteague@gmail.com>
2021-09-28 13:20:07 -0400

    Update upload-algolia.rb

--- .github/workflows/upload-algolia.rb
+++ .github/workflows/upload-algolia.rb
@@ -1,1 +1,15 @@
-puts "Hello World!"
+require 'json'
+require 'algolia
+
+apiKey = ENV["ALGOLIA_API_KEY"]
+appId = ENV["ALGOLIA_APP_ID"]
+indexName = ENV["ALGOLIA_INDEX"]
+indexFile = ENV["QUARTO_INDEX_PATH"]
+
+client  = Algolia::Search::Client.create(appId, apiKey)
+index   = client.init_index(indexName)
+file    = File.read(indexFile)
+records = JSON.parse(file)
+
+# The API client automatically batches your records
+index.save_objects(records, { autoGenerateObjectIDIfNotExist: true })
