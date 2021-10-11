4a166dbd8229b5807a65ee1a83146e15e2abed08
Charles Teague <cteague@gmail.com>
2021-10-11 16:54:56 -0400

    Update index.js

--- .github/workflows/actions/release-info/index.js
+++ .github/workflows/actions/release-info/index.js
@@ -31,7 +31,7 @@ async function run() {
   releaseInfo.assets = [];
   for (asset of releaseRaw.assets) {
     const assetFile = await octokit.rest.repos.getReleaseAsset({
-      accept: "application/octet-stream"
+      accept: "application/octet-stream",
       owner,
       repo,
       asset_id: asset.id
