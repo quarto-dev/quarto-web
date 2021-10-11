c3d992d42f6089180c2b2b94b873e868ede3fc97
Charles Teague <cteague@gmail.com>
2021-10-11 17:08:21 -0400

    fetch asset and compute sha

--- .github/workflows/actions/release-info/index.js
+++ .github/workflows/actions/release-info/index.js
@@ -1,60 +1,56 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fetch = require("node-fetch");
+const hasha = require("hasha");
 
 async function run() {
   // Repo information
-  const owner = core.getInput('owner');
-  const repo = core.getInput('repo');
+  const owner = core.getInput("owner");
+  const repo = core.getInput("repo");
 
   // Path info
-  const pathToWrite = core.getInput('out-path');
+  const pathToWrite = core.getInput("out-path");
 
   // GH Api
-  const myToken = core.getInput('github-token');
-  const octokit = github.getOctokit(myToken)
+  const myToken = core.getInput("github-token");
+  const octokit = github.getOctokit(myToken);
 
   const latestRelease = await octokit.rest.repos.getLatestRelease({
     owner,
-    repo
+    repo,
   });
-  
+
   const releaseRaw = latestRelease.data;
-  
+
   const releaseInfo = {};
-  
+
   // Release metadata
   releaseInfo.version = releaseRaw.tag_name.slice(1);
   releaseInfo.name = releaseRaw.name;
-  
+
   // Release assets
   releaseInfo.assets = [];
   for (asset of releaseRaw.assets) {
-    const assetFile = await octokit.rest.repos.getReleaseAsset({
-      accept: "application/octet-stream",
-      owner,
-      repo,
-      asset_id: asset.id
-    });
-    console.log(assetFile);
-    
-    
+    const algorithm = "sha256";
+    const assetFile = await fetch(asset.browser_download_url);
+    const buffer = await assetFile.buffer();
+    const checksum = hasha(buffer, { algorithm });
+
     releaseInfo.assets.push({
       name: asset.name,
       download_url: asset.browser_download_url,
-      checksum: "",
-      size: asset.size
+      checksum,
+      size: asset.size,
     });
   }
-  
+
   console.log(releaseInfo);
 }
 
 try {
   run().catch((error) => {
     core.setFailed(error.message);
-  }); 
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
-
