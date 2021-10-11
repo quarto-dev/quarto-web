40d10b9b85d5043a964651c06d15ae51805c25ad
Charles Teague <cteague@gmail.com>
2021-10-11 16:29:29 -0400

    Update index.js

--- .github/workflows/actions/release-info/index.js
+++ .github/workflows/actions/release-info/index.js
@@ -5,12 +5,12 @@ const github = require('@actions/github');
 async function run() {
   // Repo information
   const owner = core.getInput('owner');
-  const repo = core.getInput("name");
+  const repo = core.getInput('repo');
 
-
+  // Path info
   const pathToWrite = core.getInput('out-path');
 
-
+  // GH Api
   const myToken = core.getInput('github-token');
   const octokit = github.getOctokit(myToken)
 
