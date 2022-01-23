1f3844ae69ad10894bd3b9102c5a8dcb20dc8aa9
JJ Allaire <jj@rstudio.com>
2022-01-23 11:35:31 -0500

    re-organize htmltools code

---
+++ htmltools/gallery.R
@@ -0,0 +1,46 @@
+library(htmltools)
+
+# gallery is a list file()ories
+gallery <- function(file) {
+  gallery <- yaml::yaml.load_file(file)
+  tagList(lapply(gallery, category))
+}
+
+# category is a list of items with a header
+category <- function(category) {
+  withTags(tagList(
+    h2(category$category),
+    p(category$description),
+    div(class = "gallery-category grid",
+        lapply(category$items, item)
+    )
+  ))
+}
+
+# item is a bootstrap card with a thumbnail
+item <- function(item) {
+  withTags({
+    div(class = "card border-2 rounded-3 g-col-24 g-col-sm-12 g-col-md-8 mb-2",
+        div(class = "card-header py-1 px-2 border-bottom border-1",
+            small(class = "card-text text-nowrap inline-block",
+                  a(href = item$href, item$title), 
+                  span(" "),
+                  span(class = "fw-light", item$subtitle)
+            ),
+            a(class = "source-code card-text float-end inline-block", 
+              href = item$code,
+              title = "View source code",
+              i(class="bi-code-slash")
+            )
+        ),
+        a(href = item$href,
+          img(
+            class = "card-img-top",
+            src = item$thumbnail,
+            alt = item$description
+          )
+        )
+    )
+  })
+}
+
---
+++ htmltools/links.R
@@ -0,0 +1,18 @@
+library(htmltools)
+
+card <- function(x) {
+  div(class = "card",
+      div(class = "card-body",
+          div(class = "card-title", tags$h4(x$title)),
+          div(class = "card-subtitle text-muted", tags$h6(HTML(x$subtitle))),
+          tags$ul(tagList(lapply(x$links, function(link) { 
+            tags$li(tags$a(href = link[[2]], link[[1]]))
+          })))
+      )    
+  )
+}
+
+link_cards <- function(file) {
+  cards <- yaml::yaml.load_file(file)
+  div(class = "link-cards", tagList(lapply(cards, card)))
+}
