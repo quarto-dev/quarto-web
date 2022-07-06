1492f1bc404bc3850e73310529cf48359fb2a6c9
Charles Teague <cteague@gmail.com>
2022-07-06 11:20:50 -0400

    Update upload-algolia.rb

--- .github/workflows/upload-algolia.rb
+++ .github/workflows/upload-algolia.rb
@@ -1,10 +1,15 @@
 require 'json'
 require 'algolia'
+require 'open-uri'
 
 apiKey = ENV["ALGOLIA_API_KEY"]
 appId = ENV["ALGOLIA_APP_ID"]
 indexName = ENV["ALGOLIA_INDEX"]
 indexFile = ENV["QUARTO_INDEX_PATH"]
+
+# Download the index from quarto.org
+download = open('https://quarto.org/search.json')
+IO.copy_stream(download, indexFile)
 
 client  = Algolia::Search::Client.create(appId, apiKey)
 index   = client.init_index(indexName)
