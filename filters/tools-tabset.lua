7c461859cb8810bd82887b1be81deb9f18c1299e
J.J. Allaire <jj.allaire@gmail.com>
2022-10-18 12:09:13 -0400

    improved create website docs

--- filters/tools-tabset.lua
+++ filters/tools-tabset.lua
@@ -6,9 +6,28 @@ local kTabsetIcons = {
   ["Terminal"] = "text-editor-logo.png"
 }
 
+local injected = false
+local function injectChooseYourTool()
+  if not injected then
+    injected = true
+    quarto.doc.include_text('after-body', [[
+      <script type="text/javascript">
+        for (const navTab of document.querySelectorAll(".panel-tabset[data-group='tools-tabset'] ul")) {
+          const choose = document.createElement("h3");
+          choose.classList.add("no-anchor");
+          choose.classList.add("choose-your-tool");
+          choose.innerText = "Choose your tool";
+          navTab.prepend(choose);
+        }       
+      </script>
+    ]])
+  end
+end
+
 function Div(el)
   if el.classes:includes("panel-tabset") then
     if el.attributes["group"] == "tools-tabset" then
+      injectChooseYourTool()
       return el:walk({
         Header = function(headingEl)
           local text = pandoc.utils.stringify(headingEl.content)
